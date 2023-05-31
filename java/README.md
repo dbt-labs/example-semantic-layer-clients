ae97170a5bf88af9fc61451a58660f0cfe45c92f
Diego Fernández Giraldo <aiguo.fernandez@gmail.com>
2023-05-31 15:27:16 -0700

    Pull JDBC URL from env var and pretty print results for Java example (#2)

--- java/README.md
+++ java/README.md
@@ -2,10 +2,12 @@
 
 This basic example shows how to set up a Java project using gradle to connect to and query the Semantic Layer.
 
-To run this:
+To run this, make sure you have set the `DBT_JDBC_URL` env var and run:
 
 ```
-_JAVA_OPTIONS="--add-opens=java.base/java.nio=ALL-UNNAMED" ./gradlew run --args="<jdbc-url>"
+_JAVA_OPTIONS="--add-opens=java.base/java.nio=ALL-UNNAMED" ./gradlew run --args="'<query>'"
 ```
 
 NOTE: Arrow requires using the `--add-opens=java.base/java.nio=ALL-UNNAMED` JVM option, otherwise you will get errors.
+
+The query should also be within single quotes since it's a single argument.
--- java/src/main/java/JDBCExample.java
+++ java/src/main/java/JDBCExample.java
@@ -1,31 +1,20 @@
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
-import java.sql.ResultSetMetaData;
 import java.sql.Statement;
 import java.sql.SQLException;
-
+import org.verdictdb.commons.DBTablePrinter;
 
 class JDBCExample {
     public static void main(String[] args) {
-        String connectionUrl = args[0];       
+        String connectionUrl = System.getenv("DBT_JDBC_URL");
         try (Connection conn = DriverManager.getConnection(connectionUrl)) {
             try (Statement s = conn.createStatement()) {
-                ResultSet rs = s.executeQuery("select * from {{semantic_layer.query(metrics=['cancellation_rate'])}}");
-                ResultSetMetaData rsmd = rs.getMetaData();
-                int columnsNumber = rsmd.getColumnCount();
-                while (rs.next()) {
-                   for (int i = 1; i <= columnsNumber; i++) {
-                      if (i > 1) System.out.print(",  ");
-                      String columnValue = rs.getString(i);
-                      System.out.print(columnValue + " " + rsmd.getColumnName(i));
-                   }
-                   System.out.println("");
-                }
+                ResultSet rs = s.executeQuery(args[0]);
+                DBTablePrinter.printResultSet(rs);
             }
         } catch (SQLException e) {
             throw new RuntimeException(e);
         }
-        System.out.println("Finished");
     }
 }
